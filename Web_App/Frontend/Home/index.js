0c8a377f7a50324158f64640bd6cb6adcece09ce
LachlanMajor <majorlachie@gmail.com>
2025-12-01 19:22:45 +1100

    Home Page Settings Window Functionality initialised

---
+++ Web_App/Frontend/Home/index.js
@@ -0,0 +1,34 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+    
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    })
+
+}
+
+function toggleSubMenu(button){
+    button.nextElementSibling.classList.toggle('show')
+    button.classList.toggle('rotate')
+    if(sidebar.classList.contains('close')){
+        sidebar.classList.toggle('close')
+        toggleButton.classList.toggle('rotate')
+    }
+}
+
+document.addEventListener('click', function(event) {
+    if (!sidebar.classList.contains('close') && !sidebar.contains(event.target)) {
+        sidebar.classList.toggle('close')
+        toggleButton.classList.toggle('rotate')
+
+        Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+            ul.classList.remove('show')
+            ul.previousElementSibling.classList.remove('rotate')
+        })
+    }
+})
